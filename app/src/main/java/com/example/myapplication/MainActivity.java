f11c9c418f0120c3f9a229eac086cde6d88c21a4
ronyaryaW1 <bangwijay11@gmail.com>
2019-08-03 10:50:56 -0700

    scrollview

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+    }
+
+}
